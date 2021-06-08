e0e38402eb7f0f47d9c8c93c22da9bd07c0d2b12
ishaan <iakrules@gmail.com>
2021-06-07 21:47:23 -0400

    +subfolders

--- bot.py
+++ bot.py
@@ -18,8 +18,29 @@ async def load(ctx, extension):
 async def unload(ctx, extension):
     client.unload_extension(f'cogs.{extension}')
 
-for filename in os.listdir('./cogs'):
+# Subfolders
+for filename in os.listdir('./cogs/fun'):
     if filename.endswith('.py'):
-        client.load_extension(f'cogs.{filename[:-3]}')
+        client.load_extension(f'cogs.music.{filename[:-3]}')
+
+for filename in os.listdir('./cogs/general'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.general.{filename[:-3]}')
+
+for filename in os.listdir('./cogs/info'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.music.{filename[:-3]}')
+
+for filename in os.listdir('./cogs/moderation'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.moderation.{filename[:-3]}')
+
+for filename in os.listdir('./cogs/music'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.music.{filename[:-3]}')
+
+for filename in os.listdir('./cogs/owner'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.music.{filename[:-3]}')
 
 client.run(TOKEN)
