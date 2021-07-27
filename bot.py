47b584e0890e4ce89a4f8d6e1d963c803a03303d
rzmk <mueez25@gmail.com>
2021-07-27 13:58:39 -0400

    Comment unused cogs

--- bot.py
+++ bot.py
@@ -26,13 +26,13 @@ async def unload(ctx, extension):
     client.unload_extension(f'cogs.{extension}')
 
 # Subfolders
-for filename in os.listdir('./cogs/fun'):
-    if filename.endswith('.py'):
-        client.load_extension(f'cogs.fun.{filename[:-3]}')
+# for filename in os.listdir('./cogs/fun'):
+#     if filename.endswith('.py'):
+#         client.load_extension(f'cogs.fun.{filename[:-3]}')
 
-for filename in os.listdir('./cogs/general'):
-    if filename.endswith('.py'):
-        client.load_extension(f'cogs.general.{filename[:-3]}')
+# for filename in os.listdir('./cogs/general'):
+#     if filename.endswith('.py'):
+#         client.load_extension(f'cogs.general.{filename[:-3]}')
 
 for filename in os.listdir('./cogs/info'):
     if filename.endswith('.py'):
@@ -46,9 +46,9 @@ for filename in os.listdir('./cogs/music'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.music.{filename[:-3]}')
 
-for filename in os.listdir('./cogs/owner'):
-    if filename.endswith('.py'):
-        client.load_extension(f'cogs.owner.{filename[:-3]}')
+# for filename in os.listdir('./cogs/owner'):
+#     if filename.endswith('.py'):
+#         client.load_extension(f'cogs.owner.{filename[:-3]}')
 
 for filename in os.listdir('./cogs/inventory'):
     if filename.endswith('.py'):
